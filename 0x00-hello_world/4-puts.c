2901eab08471e11554946ae71545ea0f1c7150ff
wdianart <wairimudianart@gmail.com>
2022-04-07 14:31:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,21 +3,19 @@
 
 
 /**
-
- * main - Print a test
-
  *
-
- * Return: Always 0 (Success)
-
- */
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
