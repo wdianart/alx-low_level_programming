9bd4752ef4feb25b6da8cec9821720cc482a9883
wdianart <wairimudianart@gmail.com>
2022-04-14 02:41:33 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - tests function that prints if integer is positive or negative
+ *
+ *Return: 0
+ */
+
+int main(void)
+{vim
+int i;
+
+i=0;
+positive_or _negative(i);
+return(0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+thus read me file is about the debugging of the program C
+
