ec7a9631e95abf8131f6d433796de466010c7ec4
wdianart <wairimudianart@gmail.com>
2022-04-19 01:55:54 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <=100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
@@ -24,7 +24,7 @@ printf(" Buzz");
 else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-} 
+}
 else if (i == 1)
 {
 printf("%d", i);
