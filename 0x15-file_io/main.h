e5be2baf93e118dcb73a63a955698fcbf7cfc05b
wdianart <101327450+wdianart@users.noreply.github.com>
2022-11-02 14:42:10 +0300

    header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef FILE_H
+#define FILE_H
+/*
+ *Author: BOSTONE OCHIENG
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
